62ea9f8c5ab8ecfe5e76c60bdd110a3cc9576e89
Justin Johnson <jumejohn@live.com>
2022-09-02 11:50:23 -0700

    Update email list display

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,37 +3,45 @@ import { useDispatch, useSelector } from "react-redux";
 import Login from "./Login";
 import Logout from "./Logout";
 
-
 const Header = () => {
-  const userObject = useSelector((state) => state.userState[0])
-  if (userObject.name === ''){
-    userObject.name = "Please log in to hear your emails"
-    userObject.imageUrl = "https://cdn.dogsplanet.com/wp-content/uploads/2022/07/A-dog-floating-in-space-upscaled.jpg"
-    } 
-      
+  const userObject = useSelector((state) => state.userState[0]);
+  if (userObject.name === "") {
+    userObject.name = "Please log in to hear your emails";
+    userObject.imageUrl =
+      "https://cdn.dogsplanet.com/wp-content/uploads/2022/07/A-dog-floating-in-space-upscaled.jpg";
+  }
+
   return (
-  <div className="container-fluid">
-    <nav className="navbar navbar-expand-lg navbar-light bg-light" >
+    <div className="container-fluid">
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container">
           {/* <h1 className="navbar-brand">Email to Speech</h1> */}
           <div className="card">
             <div className="card-body">
               <h2 className="card-title">EMAIL to SPEECH</h2>
-              <h4 className="card-subtitle mb-2 text-muted">{userObject.name}</h4>
-              <img className="card-img-bottom user-image" src={userObject.imageUrl} alt={userObject.name} />
+              <h4 className="card-subtitle mb-2 text-muted">
+                {userObject.name}
+              </h4>
+              <img
+                className="card-img-bottom user-image"
+                src={userObject.imageUrl}
+                alt={userObject.name}
+              />
             </div>
           </div>
-          <div className="navbar-nav" >
-      
-          <div className="nav-item nav-btn"><Login /></div> 
-          <div className="nav-item nav-btn"><Logout /></div> 
+          <div className="navbar-nav">
+            <div className="nav-item nav-btn">
+              <Login />
+            </div>
+            <div className="nav-item nav-btn">
+              <Logout />
+            </div>
           </div>
         </div>
       </nav>
-          <User />
-  </div>
-    )
-
+      <User />
+    </div>
+  );
 };
 
 export default Header;
--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,18 +1,17 @@
-
-import React from 'react'
-import {GoogleLogout} from 'react-google-login'
-const CLIENT_ID = process.env.REACT_APP_GOOGLE_CLIENT_ID
+import React from "react";
+import { GoogleLogout } from "react-google-login";
 import { resetEmailState, unsetUser } from "../actions";
-import { useDispatch } from 'react-redux';
+import { useDispatch } from "react-redux";
+const CLIENT_ID = process.env.REACT_APP_GOOGLE_CLIENT_ID;
 
 const Logout = () => {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   const onSuccess = () => {
-    dispatch(resetEmailState())
-    dispatch(unsetUser())
-    console.log('logout successful')
-  }
-  
+    dispatch(resetEmailState());
+    dispatch(unsetUser());
+    console.log("logout successful");
+  };
+
   return (
     <div id="signOutButton">
       <GoogleLogout
