51e396eca568746ba8b9c3126cdbbaf50af057dc
Nathaniel <nathaniel.wallace@gmail.com>
2022-09-02 09:48:49 -0400

    clear state on logout working, clear state on button click working

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import "./App.css";
 import Header from "./components/Header";
-import User from "./components/User";
 import DisplayEmails from "./containers/Dispaly_emails";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      {/* <User /> */}
       <DisplayEmails />
     </div>
   );
--- src/components/Logout.js
+++ src/components/Logout.js
@@ -2,10 +2,12 @@ import React from 'react'
 import {GoogleLogout} from 'react-google-login'
 const CLIENT_ID = process.env.REACT_APP_GOOGLE_CLIENT_ID
 import { resetEmailState } from "../actions";
+import { useDispatch } from 'react-redux';
 
 const Logout = () => {
+  const dispatch = useDispatch()
   const onSuccess = () => {
-    resetEmailState()
+    dispatch(resetEmailState())
     console.log('logout successful')
   }
   return (
