5d076510fe89722b2d9cb403651219c738916a6c
Justin Johnson <jumejohn@live.com>
2022-09-02 08:44:42 -0700

    update some code styling, grammar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,27 @@
 import User from "./User";
-import Container from 'react-bootstrap/Container';
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Login from "./Login";
+import Logout from "./Logout";
 
 const Header = () => {
   return (
-
-  <Container>
-     <Navbar expand="lg" variant="light" bg="light">
+    <Container>
+      <Navbar expand="lg" variant="light" bg="light">
         <Container>
-          <Navbar.Brand >Email to Speech</Navbar.Brand>
-          <Nav><Login /></Nav> 
-          <Nav><Logout /></Nav> 
+          <Navbar.Brand>Email to Speech</Navbar.Brand>
+          <Nav>
+            <Login />
+          </Nav>
+          <Nav>
+            <Logout />
+          </Nav>
         </Container>
       </Navbar>
-          <User />
-  </Container>
-    )
+      <User />
+    </Container>
+  );
 };
 
 export default Header;
